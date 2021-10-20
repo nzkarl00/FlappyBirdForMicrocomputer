a5e2966d83f77ca69d0af63429a229f9d3665b17
Finn van Dorsser <faziolidorsser@gmail.com>
2021-10-21 11:36:54 +1300

    Removed commented code in update_position

--- final/player.c
+++ final/player.c
@@ -34,9 +34,7 @@ void player_duck(player_t* player) {
 }
 
 void update_position(player_t* player) {
-    /* if (navswitch_push_event_p (NAVSWITCH_EAST)) {
-        player_duck(player);
-    } else  */if (navswitch_push_event_p (NAVSWITCH_WEST)) {
+    if (navswitch_push_event_p (NAVSWITCH_WEST)) {
         player_jump(player);
     }
 }
